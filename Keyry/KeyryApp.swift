9eb671cf2359efbd52bdf0b07c7594b10611e4bf
Linda Lau <lindalau81@gmail.com>
2024-02-21 14:44:08 -0700

    Initial Commit

---
+++ Keyry/KeyryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyryApp.swift
+//  Keyry
+//
+//  Created by Linda Lau on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
