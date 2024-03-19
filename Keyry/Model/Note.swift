fbb7a042e8e00d9be3ca260c2776f88948e9f445
Linda Lau <lindalau81@gmail.com>
2024-03-18 18:35:55 -0700

    update models

--- Keyry/Model/Note.swift
+++ Keyry/Model/Note.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+class Note: ObservableObject{
+  //let dateCreated = Date()
+  @Published var keywords: [String] = []
+  //@Published var lastEdited = Date()
+  @Published var content = ""
+  @Published var title: String
+
+  init(_ name: String) {
+    self.title = name
+  }
+
+  func isKeywordUnique(_ word: String) -> Bool {
+    !keywords.contains(word)
+  }
+
+  func addKeywords(_ words: [String]) -> Bool{
+    let duplicates = Set(keywords).intersection(Set(words))
+    guard duplicates.isEmpty else {
+      // throw a constructed error with all of the duplicate keywords
+      return false
+    }
+
+    keywords += words
+    return true
+  }
+
+  func removeKeywords(_ words: [String]) {
+    keywords.removeAll { keyword in
+      words.contains(keyword)
+    }
+  }
+
+//  func addKeyword(_ word: String) -> Bool {
+//    guard isKeywordUnique(word) else {
+//      return false
+//    }
+//    keywords.append(word)
+//    return true
+//  }
+}
+
+extension Note: Hashable {
+  static func == (lhs: Note, rhs: Note) -> Bool {
+    return lhs.content == rhs.content && lhs.title == rhs.title
+  }
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(content)
+  }
+}
